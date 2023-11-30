4996d05e8e23a9656c4203c53e7631341ad8a0e6
Emi Cabanillas <emicabanillas.cab@gmail.com>
2023-11-29 23:31:15 -0300

    add handlerSumit

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 const Login = () => {
+    const [login, setLogin] = useState({})
+
+    function handlerSubmit(e) {
+        e.preventDefault();
+        const usuario = {
+            usuario: e.target.usuario.value,
+            password: e.target.password.value,
+        }
+        setLogin(usuario);
+    }
+    console.log(login);
+
     return (
         <div className='content'>
             <h1>inicia sesión</h1>
-            <form>
+            <form onSubmit={handlerSubmit}>
                 <div className='content_label'>
                     <label className='form_label'>
                         <span className='label_span'>Usuario</span>
